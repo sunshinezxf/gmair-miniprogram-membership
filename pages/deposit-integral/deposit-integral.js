ad297a993fd875b583fab28496d5a37bbd9f1f1e
JobyTerry <1727177598@qq.com>
2021-10-14 14:51:26 +0800

    积分模块完成

--- pages/deposit-integral/deposit-integral.js
+++ pages/deposit-integral/deposit-integral.js
@@ -7,6 +7,8 @@ Page({
    */
   data: {
     fileList: [],
+    deviceModel: '',
+    description: ''
   },
 
   /**
@@ -16,66 +18,132 @@ Page({
 
   },
   beforeRead(event) {
-    const { file, callback } = event.detail;
+    const {
+      file,
+      callback
+    } = event.detail;
     callback(file.type === 'image');
   },
   afterRead(event) {
-    const { file } = event.detail;
+    const {
+      file
+    } = event.detail;
     console.log(file);
-    const { fileList = [] } = this.data;
-    fileList.push({ url: file.url,deletable:true });
-    this.setData({ fileList });
+    var that = this;
     // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
-    // wx.uploadFile({
-    //   url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
-    //   filePath: file.url,
-    //   name: 'file',
-    //   formData: { user: 'test' },
-    //   success(res) {
-    //     // 上传完成需要更新 fileList
-    //     const { fileList = [] } = this.data;
-    //     fileList.push({ ...file, url: res.data });
-    //     this.setData({ fileList });
-    //   },
-    // });
+    wx.uploadFile({
+      url: 'http://192.168.50.217:9017/resource/image/upload', // 仅为示例，非真实的接口地址
+      filePath: file.url,
+      name: 'image',
+      success(res) {
+        console.log("JSON.parse(res.data).data.fileUrl",JSON.parse(res.data).data.fileUrl);
+        // 上传完成需要更新 fileList
+        const {
+          fileList = []
+        } = that.data;
+        fileList.push({
+          url: JSON.parse(res.data).data.fileUrl,
+          deletable: true
+        });
+        that.setData({
+          fileList
+        });
+      },
+    });
   },
-  deletePhone(event){
+  deletePhone(event) {
     let index = event.detail.index;
-    const { fileList = [] } = this.data;
-    fileList.splice(index,1);
-    this.setData({ fileList });
+    const {
+      fileList = []
+    } = this.data;
+    fileList.splice(index, 1);
+    this.setData({
+      fileList
+    });
   },
-  oversize(event){
+  oversize(event) {
     wx.showToast({
       title: '请上传图片小于10M',
-      icon:"none"
+      icon: "none"
     })
   },
-  formSubmit(e) {
-    console.log(e.detail.value.integral);
-    console.log(e.detail.value.description);
+  onDeviceModelChange(event) {
+    this.setData({
+      deviceModel:event.detail
+    });
+  },
+  onRemarkChange(event) {
+    this.setData({
+      description:event.detail
+    });
+  },
+
+  dataSubmit(e) {
+    if(this.data.fileList.length==0){
+      wx.showToast({
+        title: '请上传至少一张图片',
+        icon:"none"
+      });
+      return;
+    }
+    if(this.data.deviceModel==null||this.data.deviceModel.length==0){
+      wx.showToast({
+        title: '设备型号不可为空',
+        icon:"none"
+      });
+      return;
+    }
+
     var that = this;
     wx.showLoading();
     var params = {
-      url: "/p/integral/deposit",
+      url: "/p/integral/supplementaryIntegral",
       method: "POST",
       data: {
-        integral:e.detail.value.integral,
-        description:e.detail.value.description
+        pictures: that.getPictures(),
+        deviceModel: that.data.deviceModel,
+        description: that.data.description
       },
       callBack: function (res) {
         wx.hideLoading();
         wx.showToast({
           title: '提交成功',
-        })
+        });
+        that.setData({
+          fileList: [],
+          deviceModel: '',
+          description: ''
+        });
       }
     };
     http.request(params);
   },
-
-
-
-
+  getPictures:function(){
+    let pictureList = this.data.fileList;
+    var pictures = '';
+    for(let i=0,len=pictureList.length;i<len;i++){
+      pictures += pictureList[i].url;
+      if(i!=len-1){
+        pictures += ',';
+      }
+    }
+    return pictures;
+  },
+  scanCall:function(){
+    var that = this;
+    // 允许从相机和相册扫码
+    wx.scanCode({
+      success (res) {
+        console.log(res);
+        let scanResult = res.result;
+        let index = scanResult.lastIndexOf("\/");
+        let lastStr = scanResult.substring(index + 1,scanResult.length);
+        that.setData({
+          deviceModel:lastStr
+        });
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
